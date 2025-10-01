d62e9e692cc2cce83791df33b7f19df7a6365903
Andrey Prikhodko <5ray69@ukr.net>
2025-10-01 23:00:59 +0300

    AssemblyInfo 2.0.0.10

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("2.0.0.9")]
-[assembly: AssemblyFileVersion("9.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.10")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
