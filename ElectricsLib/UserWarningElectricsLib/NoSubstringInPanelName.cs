7a74fc9b208b1e10a054823793e301dd93f020f5
Andrey Prikhodko <5ray69@ukr.net>
2025-10-05 23:29:58 +0300

    add UserWarningElectricsLib class NoSubstringInPanelName

---
+++ ElectricsLib/UserWarningElectricsLib/NoSubstringInPanelName.cs
@@ -0,0 +1,17 @@
+﻿namespace Libraries.ElectricsLib.UserWarningElectricsLib
+{
+    public class NoSubstringInPanelName
+    {
+        public string MessageForUser(string substringInName)
+        {
+            string message = $@"
+В проекте нет панели с подстрокой в имени
+{substringInName}.
+
+Разместите семейство с такой подстрокой в имени или измените
+имя панели существующего семейства на соответствующее.";
+
+            return message;
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("2.0.0.11")]
-[assembly: AssemblyFileVersion("10.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.12")]
+[assembly: AssemblyFileVersion("12.0.0.0")]
