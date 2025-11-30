7f4cce941999c66e3735c85c03f29be6e19b061c
Andrey Prikhodko <5ray69@ukr.net>
2025-11-30 13:59:09 +0200

    AssemblyVersion("3.0.0.28")

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("3.0.0.27")]
-[assembly: AssemblyFileVersion("27.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.28")]
+[assembly: AssemblyFileVersion("28.0.0.0")]
